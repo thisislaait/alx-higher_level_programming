8f7c3d87478c33419698e707f06b0fa334108052
thisislaait <ogadi.ugo@gmail.com>
2024-01-22 18:00:07 +0100

    Fix: division line script format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    if(my_list):
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end='')
-                printed += 1
-            except IndexError:
-                print()
-                return(printed)
-    print()
-    return(printed)
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (printed)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     newList = []
-    for x in range(list_length):
-        result = 0
+    for x in range(0, list_length):
         try:
-            result = my_list_1[x] / my_list_2[x]
+            div = my_list_1[x] / my_list_2[x]
         except IndexError:
-            print("{}".format("out of range"))
+            print("out of range")
+            div = 00
         except (ValueError, TypeError):
-            print("{}".format("wrong type"))
+            print("wrong type")
+            div = 0
         except ZeroDivisionError:
-            print("{}".format("division by 0"))
+            print("division by 0")
+            div = 0
         finally:
-            newList.append(result or 0)
+            newList.append(div)
     return(newList)
