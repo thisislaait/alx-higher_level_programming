db53ed36b54b4396310fff44f7c16c9e88d58f34
Ugo Laait <140851946+thisislaait@users.noreply.github.com>
2024-01-23 02:34:57 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    ret = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    return (ret)
+    printed_elements = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_elements += 1
+    except IndexError:
+        pass
+
+    print()
+    return printed_elements
