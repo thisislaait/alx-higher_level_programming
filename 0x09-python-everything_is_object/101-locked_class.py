4737d6cc1742845c79364d0ed1f86c7d32d419a2
thisislaait <ogadi.ugo@gmail.com>
2024-01-31 00:34:33 +0100

    make corrections

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Defines a locked class"""
 
 class LockedClass:
+    """
+    This locked class will take ONLY attributes called 'first_name'
+    """
+
     __slots__ = ['first_name']
