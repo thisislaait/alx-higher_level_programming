13d56778ae21415361351d57e7b78f0a607cd3c9
thisislaait <ogadi.ugo@gmail.com>
2024-02-03 02:35:41 +0100

    Fix: restore

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,61 +1,47 @@
 #!/usr/bin/python3
-"""Unittests for max_integer([..])."""
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-
-
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
 
-    def test_ordered_list(self):
-        """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+    def test_one(self):
+        matrix1 = [1,2,3,4]
+        self.assertEqual(max_integer(matrix1),4)
 
-    def test_unordered_list(self):
-        """Test an unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
+    def test_two(self):
+        matrix2 = []
+        self.assertEqual(max_integer(matrix2),None)
 
-    def test_max_at_begginning(self):
-        """Test a list with a beginning max value."""
-        max_at_beginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(max_at_beginning), 4)
+    def test_three(self):
+        self.assertRaises(TypeError, max_integer,0)
 
-    def test_empty_list(self):
-        """Test an empty list."""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
+    def test_four(self):
+        matrix4 = {5:'a',4:'b',3:'c'}
+        self.assertRaises(KeyError, max_integer,matrix4)
 
-    def test_one_element_list(self):
-        """Test a list with a single element."""
-        one_element = [7]
-        self.assertEqual(max_integer(one_element), 7)
+    def test_five(self):
+        matrix = ['a','b','c']
+        self.assertEqual(max_integer(matrix),'c')
 
-    def test_floats(self):
-        """Test a list of floats."""
-        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
-        self.assertEqual(max_integer(floats), 15.2)
+    def test_six(self):
+        matrix = 'hello'
+        self.assertEqual(max_integer(matrix),'o')
 
-    def test_ints_and_floats(self):
-        """Test a list of ints and floats."""
-        ints_and_floats = [1.53, 15.5, -9, 15, 6]
-        self.assertEqual(max_integer(ints_and_floats), 15.5)
+    def test_seven(self):
+        matrix = [-500,1,2,3]
+        self.assertEqual(max_integer(matrix),3)
 
-    def test_string(self):
-        """Test a string."""
-        string = "Ugo"
-        self.assertEqual(max_integer(string), 'r')
+    def test_eight(self):
+        matrix1 = [4]
+        self.assertEqual(max_integer(matrix1),4)
 
-    def test_list_of_strings(self):
-        """Test a list of strings."""
-        strings = ["Ugo", "is", "my", "name"]
-        self.assertEqual(max_integer(strings), "name")
+    def test_nine(self):
+        matrix1 = [4,1,2,3]
+        self.assertEqual(max_integer(matrix1),4)
 
-    def test_empty_string(self):
-        """Test an empty string."""
-        self.assertEqual(max_integer(""), None)
+    def test_ten(self):
+        matrix1 = [1,2,6,5,4]
+        self.assertEqual(max_integer(matrix1),6)
 
 if __name__ == '__main__':
     unittest.main()
+
