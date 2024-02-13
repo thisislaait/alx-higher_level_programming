09d44e534e7305e043ec11c73a59dc803b2fb9f7
thisislaait <ogadi.ugo@gmail.com>
2024-02-13 03:16:55 +0100

    Fix: retest and update error

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -98,7 +98,7 @@ class TestSquare(unittest.TestCase):
         r1.update(89, 2, 3)
         self.assertEqual(r1.__str__(), "[Square] (89) 3/10 - 2")
         r1.update(x=1, height=2, y=3, width=4)
-        self.assertEqual(r1.__str__(), "[Square] (89) 1/3 - 4")
+        self.assertEqual(r1.__str__(), "[Square] (89) 1/3 - 2")
         with self.assertRaises(ValueError, msg="x must be >=0"):
             r1.update(y=1, width=2, x=-3, id=89)
         r2 = Square(2, 2)
